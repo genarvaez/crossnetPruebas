786c3c31ec2d9336210e35780fe2e7caf9b758ff
genarvaez <ge_narvaez@hotmail.com>
2017-11-08 12:54:22 -0300

    carousel terminado, secciones modulares en proceso sin contenido

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,17 +1,12 @@
 $( document ).ready(function(){
+	$('.carousel.carousel-slider').carousel({fullWidth: true});
+	setInterval(function(){
+		$('.carousel').carousel('next');
+	}, 6000);
 	var buttonsubMenu = $(".sub-menu-button");
-	var subMenuOption = $(".sub-menu-option");
 	buttonsubMenu.click(function(){
-		
 		$(".sub-menu-option").toggle("fast", function(){
-			$(this).addClass("bounceIn")
+			$(this).addClass("bounceIn");
 		})
 	})
-
-	/*buttonsubMenu.mouseleave(function(){
-		$(".sub-menu-contact").addClass("hide");
-		$(".sub-menu-option").removeClass("bounceIn")
-	})*/
-
-
 })
