b8a42cb9a39be04ea2158d0dddec7dbe339980e2
genarvaez <ge_narvaez@hotmail.com>
2017-11-08 17:24:53 -0300

    maqueta inicial con espacios vacios, inicia pruebas WRTC

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,12 +1,25 @@
 $( document ).ready(function(){
+
+	var buttonsubMenu = $(".sub-menu-button");
+	var videoLLamadaButton = $(".video-llamada-on");
+
+
 	$('.carousel.carousel-slider').carousel({fullWidth: true});
+	$(".button-collapse").sideNav();
 	setInterval(function(){
 		$('.carousel').carousel('next');
 	}, 6000);
-	var buttonsubMenu = $(".sub-menu-button");
+
+	
 	buttonsubMenu.click(function(){
 		$(".sub-menu-option").toggle("fast", function(){
 			$(this).addClass("bounceIn");
+		})
+	})
+
+	videoLLamadaButton.click(function(){
+		$(".video-call-section").toggle("fast", function(){
+			
 		})
 	})
 })
